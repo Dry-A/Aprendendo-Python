73b1510567886b7d86a7da0982a11cd7df01fd11
Audrey-Dev-Full-Stack <audreydev.br@gmail.com>
2024-02-20 23:21:58 -0300

    :cat: Exercicio com funcoes e objetos

--- Lista_exercícios_lógica_em_python/main.py
+++ Lista_exercícios_lógica_em_python/main.py
@@ -30,7 +30,7 @@ def informa_maioridade(lista_idades):
     lista_maioridade = []
   
     for pessoa in lista_idades:
-        nome = pessoa['nome'],
+        nome = pessoa['nome']
         idade = pessoa['idade']
         
         if idade >= 18:
@@ -45,16 +45,64 @@ def informar_sexo(lista):
     lista_nova =[]
     
     for pessoa in lista:
-        nome = pessoa['nome'],
+        nome = pessoa['nome']
         sexo = pessoa['sexo']
         
         if sexo == 'feminino':
-            nome = pessoa['nome'],
+            nome = pessoa['nome']
             genero = 'mulher'
             lista_nova.append(pessoa)
         else:
-            nome = pessoa['nome'],
+            nome = pessoa['nome']
             genero = 'homem'
             lista_nova.append(pessoa)
     
     return lista_nova
+
+#20
+
+def exibe_valor_desconto(valor_original, percentual_desconto):
+    valor_do_desconto = valor_original *percentual_desconto
+    print("Valor do desconto é ", valor_do_desconto)
+    return valor_do_desconto
+
+
+
+def calcula_novo_valor_com_desconto(valor_original, percentual_desconto):
+    valor_do_desconto= exibe_valor_desconto(valor_original, percentual_desconto)
+    novo_valor_carro = valor_original - valor_do_desconto
+    print("O valor do carro com desconto é ", novo_valor_carro)
+    return novo_valor_carro
+
+
+def calcula_percentual_desconto(lista_carros):
+    lista_valores_com_desconto = []
+    
+    for carro in lista_carros:
+        modelo = carro['modelo']
+        ano = carro['ano']
+        valor = carro['valor']        
+        
+        if ano <=2000:
+            percentual = 12/100
+            modelo = carro['modelo']
+            valor = carro['valor']
+            ano = carro['ano']
+            novo_valor = calcula_novo_valor_com_desconto(valor, percentual)
+            lista_valores_com_desconto.append(carro)
+        elif ano> 2000:
+            percentual = 7/100
+            modelo = carro['modelo']
+            valor = carro['valor']
+            ano = carro['ano']
+            novo_valor = calcula_novo_valor_com_desconto(valor, percentual)
+    
+    return novo_valor
+
+         
+            
+            
+        
+            
+        
+            

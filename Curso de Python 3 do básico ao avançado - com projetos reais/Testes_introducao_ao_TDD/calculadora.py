a00b6a992ce2f1d3d9eab998ddf086299d6b841d
Audrey-Dev-Full-Stack <audreydev.br@gmail.com>
2024-02-25 22:23:31 -0300

    :woman_juggling: Assertions e Doctest

--- Curso de Python 3 do básico ao avançado - com projetos reais/Testes_introducao_ao_TDD/calculadora.py
+++ Curso de Python 3 do básico ao avançado - com projetos reais/Testes_introducao_ao_TDD/calculadora.py
@@ -1,12 +1,34 @@
 def soma(a,b):
-     #verifica se a é uma instancia de int ou float?
-    assert isinstance(a,(int,float)), 'a precisa ser int ou float fii'
-    assert isinstance(b,(int,float)), 'a precisa ser int ou float fii'
+    """Soma a e b
+    >>> soma(10,20)
+    30 
+    
+    >>> soma(-15,5)
+    -10
+    >>> soma('10', 20)
+    Traceback (most recent call last):
+    ...
+    AssertionError: a precisa ser int ou float
+    """
+     #verifica se a e b é uma instancia de int ou float
+    assert isinstance(a,(int,float)), 'a precisa ser int ou float'
+    assert isinstance(b,(int,float)), 'b precisa ser int ou float'
     return a+b
 
-def divisao(a,b):
-    assert isinstance(a, (int, float))  , 'a precisa ser int ou float'
-    assert isinstance(b, (int, float))  and b != 0 , 'b precisa ser int ou float e precisa ser diferente de Zero'
-    return a/b
-
 #assert náo é pro ususario final, e sim, pra outros devs saberem
+
+def subtrai(a,b):
+    """Subtrai a e b
+    
+    >>> subtrai(10,8)
+    2
+    >>> subtrai('10', 2)
+    """
+    assert isinstance(a,(int,float)), 'a precisa ser int ou float'
+    assert isinstance(b,(int,float)), 'b precisa ser int ou float'
+    return a-b
+
+#para rodar o teste da linha 2 diretamente aqui
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod(verbose=True)

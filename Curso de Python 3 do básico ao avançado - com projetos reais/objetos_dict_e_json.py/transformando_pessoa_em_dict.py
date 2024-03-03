0b69b7d422b181fdcacf5328a446d19ab8a92d87
Audrey-Dev-Full-Stack <audreydev.br@gmail.com>
2024-03-02 22:40:05 -0300

    :bulb: Demonstrando como acessar valores em dict e em objetos

--- Curso de Python 3 do básico ao avançado - com projetos reais/objetos_dict_e_json.py/transformando_pessoa_em_dict.py
+++ Curso de Python 3 do básico ao avançado - com projetos reais/objetos_dict_e_json.py/transformando_pessoa_em_dict.py
@@ -1,14 +1,21 @@
 from pessoa import Pessoa
 import json
 
+
+def faz_tracinhos():
+    print(15*"- ")
+
 pessoa_1 = Pessoa("Noruega", "branca","5002356A",52,"M")
-print(15*"- ")
+
 print("Imprime o self.blablabla :", vars(pessoa_1))
+print(f"Acessando registro no objeto - com o . ponto: {pessoa_1.registro}")
+faz_tracinhos()
 
 pessoa_1_dict = pessoa_1.to_dict()
 
-print(15*"- ")
 print("Pessoa dict: imprime como esta definido no dict: ", pessoa_1_dict)
+print(f"Acessando um registro no dict - com colchetes []: {pessoa_1_dict['registro']}")
+faz_tracinhos()
 
 #agora vamos serializar em json
 
@@ -18,6 +25,5 @@ print("Pessoa dict: imprime como esta definido no dict: ", pessoa_1_dict)
 
 pessoa_1_json = json.dumps(pessoa_1_dict)
 
-print(15*"- ")
 print("Imprimindo agora Pessoa em json ( só depois de passar para dict) :",pessoa_1_json)
 

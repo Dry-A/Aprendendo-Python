a2c2560807fe2f6729de2298a4cf78f5bbfbb959
Audrey-Dev-Full-Stack <audreydev.br@gmail.com>
2024-03-04 22:05:05 -0300

    :boomerang: objetos

--- Curso de Python 3 do básico ao avançado - com projetos reais/objetos_dict_e_json.py/instanciando_animal.py
+++ Curso de Python 3 do básico ao avançado - com projetos reais/objetos_dict_e_json.py/instanciando_animal.py
@@ -3,13 +3,17 @@ from animal import Animal
 meu_animal = Animal("mamífero","cachorro", "Dankan",4, ["raçao Proplan", "Carne","Verduras"] )
 meu_passaro = Animal("pássaro", "papagaio","Curupato", 8, ["legumes", "alpiste", "semente de girassol"])
 
+meu_peixe = Animal("peixator", "peixe", "Ferdinando", 11, ["comida de peixe", "alpiste", "farinha", "farofa"])
+
 print(meu_animal)# nao vai - imprime: <animal.Animal object at 0x000001D3615F5B80>
 
 print("Meu animal: ", vars(meu_animal))
+print("Meu peixe: ", vars(meu_peixe))
+
+del meu_animal.nome
 
 print(meu_animal)# # Imprimindo o objeto inteiro usando o método __str__ que está na classe(parece um dto)
 print(meu_passaro)
 
 print(meu_passaro.nome)
 
-print(meu_animal.nome)
